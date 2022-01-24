db76d4d329b2686bdeaccae2378382f262619da6
nikolayhotim1 <hotim_nikolay@mail.ru>
2022-01-24 20:50:47 +0300

    feature: create function component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
+import Counter from "./components/Counter";
 
 function App() {
-	const [likes, setLikes] = useState(5);
-	const [value, setValue] = useState('Text in input');
-
-	function increment() {
-		setLikes(likes + 1);
-	}
-
-	function decrement() {
-		setLikes(likes - 1);
-	}
-
 	return (
 		<div className="App">
-			<h1>{likes}</h1>
-			<h2>{value}</h2>
-
-			<input
-				type='text'
-				value={value}
-				onChange={event => setValue(event.target.value)}
-			/>
-
-			<button onClick={increment}>Increment</button>
-			<button onClick={decrement}>Decrement</button>
+			<Counter />
+			<Counter />
+			<Counter />
 		</div>
 	);
 }
