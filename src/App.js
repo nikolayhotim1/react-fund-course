4f769bada4573a2125d7223de43fc851935a5cf0
nikolayhotim1 <hotim_nikolay@mail.ru>
2022-01-26 01:05:22 +0300

    feature: ability get data from input in console

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React, { useState } from 'react';
 import PostList from './components/PostList';
+import MyButton from './components/UI/button/MyButton';
+import MyInput from './components/UI/input/MyInput';
 import './styles/App.css';
 
 const App = () => {
-	const [posts1, setPosts1] = useState([
+	const [posts, setPosts] = useState([
 		{ id: 1, title: 'JavaScript 1', body: 'Description 1' },
 		{ id: 2, title: 'JavaScript 2', body: 'Description 2' },
 		{ id: 3, title: 'JavaScript 3', body: 'Description 3' }
 	]);
 
-	const [posts2, setPosts2] = useState([
-		{ id: 1, title: 'TypeScript 1', body: 'Description 1' },
-		{ id: 2, title: 'TypeScript 2', body: 'Description 2' },
-		{ id: 3, title: 'TypeScript 3', body: 'Description 3' }
-	]);
+	const [title, setTitle] = useState('');
+
+	const addNewPost = (e) => {
+		e.preventDefault();
+		console.log(title);
+	}
 
 	return (
 		<div className='app'>
-			<PostList posts={posts1} title='Posts list 1' />
-			<PostList posts={posts2} title='Posts list 2' />
+			<form>
+				<MyInput
+					value={title}
+					onChange={e => setTitle(e.target.value)}
+					type='text'
+					placeholder='Post title'
+				/>
+				<MyInput type='text' placeholder='Post description' />
+				<MyButton onClick={addNewPost}>Create post</MyButton>
+			</form>
+
+			<PostList posts={posts} title='Posts list about JS' />
 		</div>
 	);
 };
