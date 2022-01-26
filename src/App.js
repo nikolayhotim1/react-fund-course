560338374b82badabf0a32bc8ad16c2d4960c142
nikolayhotim1 <hotim_nikolay@mail.ru>
2022-01-26 20:33:33 +0300

    refactor: create setPost function in hook

--- src/App.js
+++ src/App.js
@@ -11,21 +11,12 @@ const App = () => {
 		{ id: 3, title: 'JavaScript 3', body: 'Description 3' }
 	]);
 
-	const [title, setTitle] = useState('');
-	const [body, setBody] = useState('');
+	const [post, setPost] = useState({ title: '', body: '' });
 
 	const addNewPost = (e) => {
 		e.preventDefault();
-
-		const newPost = {
-			id: Date.now(),
-			title,
-			body
-		};
-
-		setPosts([...posts, newPost]);
-		setTitle('');
-		setBody('');
+		setPosts([...posts, { ...post, id: Date.now() }]);
+		setPost({ title: '', body: '' });
 	};
 
 	return (
@@ -33,15 +24,15 @@ const App = () => {
 			<form>
 				<MyInput
 					autoFocus
-					value={title}
-					onChange={e => setTitle(e.target.value)}
+					value={post.title}
+					onChange={e => setPost({ ...post, title: e.target.value })}
 					type='text'
 					placeholder='Post title'
 				/>
 
 				<MyInput
-					value={body}
-					onChange={e => setBody(e.target.value)}
+					value={post.body}
+					onChange={e => setPost({ ...post, body: e.target.value })}
 					type='text'
 					placeholder='Post description'
 				/>
