896f0a607cb1c1ee2e1be19c0e8ce0bb30f34fcc
nikolayhotim1 <hotim_nikolay@mail.ru>
2022-01-30 13:59:52 +0300

    feature: immediate loading of posts with animation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import PostService from './API/PostService';
 import PostFilter from './components/PostFilter';
 import PostForm from './components/PostForm';
 import PostList from './components/PostList';
 import MyButton from './components/UI/button/MyButton';
+import Loader from './components/UI/Loader/Loader';
 import MyModal from './components/UI/MyModal/MyModal';
 import { usePosts } from './hooks/usePosts';
 import './styles/App.css';
@@ -12,10 +14,24 @@ const App = () => {
 	const [filter, setFilter] = useState({ sort: '', query: '' });
 	const [modal, setModal] = useState(false);
 	const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+	const [isPostLoading, setIsPostLoading] = useState(false);
+
+	useEffect(() => {
+		fetchPosts();
+	}, [])
 
 	const createPost = (newPost) => {
 		setPosts([...posts, newPost]);
 		setModal(false);
+	};
+
+	const fetchPosts = async () => {
+		setIsPostLoading(true);
+		setTimeout(async () => {
+			const posts = await PostService.getAll();
+			setPosts(posts);
+			setIsPostLoading(false);
+		}, 1000);
 	};
 
 	const removePost = (post) => {
@@ -24,6 +40,8 @@ const App = () => {
 
 	return (
 		<div className='app'>
+			<button onClick={fetchPosts}>GET POSTS</button>
+
 			<MyButton
 				style={{ marginTop: 30 }}
 				onClick={() => setModal(true)}>
@@ -40,11 +58,20 @@ const App = () => {
 				setFilter={setFilter}
 			/>
 
-			<PostList
-				remove={removePost}
-				posts={sortedAndSearchedPosts}
-				title='Posts about JS'
-			/>
+			{isPostLoading
+				? <div style={{
+					display: 'flex',
+					justifyContent: 'center',
+					marginTop: 50
+				}}>
+					<Loader />
+				</div>
+				: <PostList
+					remove={removePost}
+					posts={sortedAndSearchedPosts}
+					title='Posts about JS'
+				/>
+			}
 		</div>
 	);
 };
---
+++ src/components/UI/Loader/Loader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import cl from './loader.module.css';
+
+const Loader = (props) => {
+	return (
+		<div className={cl.loader}></div>
+	);
+};
+
+export default Loader;
